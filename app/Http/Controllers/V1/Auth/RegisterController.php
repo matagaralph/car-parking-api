d83eec757ee77443464d75c3780282c776a0ec31
matagaralph <matagaralph@gmail.com>
2025-07-07 07:48:51 +0200

    add user registration

--- app/Http/Controllers/V1/Auth/RegisterController.php
+++ app/Http/Controllers/V1/Auth/RegisterController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers\V1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
-class RegisterController extends Controller
-{
+class RegisterController extends Controller {
     public function __invoke(Request $request) {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        
+
+        $device = substr($request->userAgent() ?? '', 0, 255);
+
+        return response()->json([
+            'access_token' => $user->createToken($device)->plainTextToken,
+        ], ResponseAlias::HTTP_CREATED);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/register', RegisterController::class);
