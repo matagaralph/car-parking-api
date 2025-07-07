dfa601e97ecd8eec4ada29aa81c41e23a6b309ec
matagaralph <matagaralph@gmail.com>
2025-07-07 07:40:23 +0200

    configure api versioning

---
+++ app/Http/Controllers/V1/Auth/RegisterController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request) {
+
+    }
+}
