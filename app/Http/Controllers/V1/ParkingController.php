7c11b1296055926ba69944ec6201117520d3c7dd
matagaralph <matagaralph@gmail.com>
2025-07-07 11:16:09 +0200

    add ability to calculate parking price

--- app/Http/Controllers/V1/ParkingController.php
+++ app/Http/Controllers/V1/ParkingController.php
@@ -5,11 +5,29 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ParkingResource;
 use App\Models\Parking;
+use App\Services\ParkingPriceService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ParkingController extends Controller {
+
+    public function index()
+    {
+        $activeParkings = Parking::active()->latest('start_time')->get();
+
+        return ParkingResource::collection($activeParkings);
+    }
+    public function history()
+    {
+        $stoppedParkings = Parking::stopped()
+            ->with(['vehicle' => fn ($q) => $q->withTrashed()])
+            ->latest('stop_time')
+            ->get();
+
+        return ParkingResource::collection($stoppedParkings);
+    }
+
     public function start(Request $request) {
         $parkingData = $request->validate([
             'vehicle_id' => [
@@ -33,12 +51,18 @@ class ParkingController extends Controller {
     }
 
     public function show(Parking $parking) {
+        $parking->load(['vehicle' => fn ($q) => $q->withTrashed()]);
         return ParkingResource::make($parking);
     }
 
     public function stop(Parking $parking) {
+        if ($parking->stop_time) {
+            return response()->json(['errors' => ['general' => ['Parking already stopped.']],]
+                , ResponseAlias::HTTP_UNPROCESSABLE_ENTITY);
+        }
         $parking->update([
             'stop_time' => now(),
+            'total_price' => ParkingPriceService::calculatePrice($parking->zone_id, $parking->start_time),
         ]);
 
         return ParkingResource::make($parking);
